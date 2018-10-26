97507876e814e8bb051c4e62253e6753a10dd250
Andrew Bregger <adb3649@tamu.edu>
2018-10-25 22:50:00 -0500

    Updating expression

--- Mist/src/frontend/parser/ast/ast_expr.cpp
+++ Mist/src/frontend/parser/ast/ast_expr.cpp
@@ -1,1 +1,119 @@
 #include "ast_expr.hpp"
+
+namespace ast {
+	ExprKind Expr::kind();
+	Type* Expr::type();
+	mist::Pos Expr::pos();
+	Expr::Expr(ExprKind k, mist::Pos p) {
+
+	}
+
+	Value::Value(Ident* name) : Decl(, pos) {
+
+	}
+	
+	Tuple::Tuple(const std::vector<Expr*>& values, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	IntegerConstExpr::IntegerConstExpr(i64 val, ConstantType cty, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	FloatConstExpr::FloatConstExpr(f64 val, ConstantType cty, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	StringConstExpr::StringConstExpr(const std::string& val, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	BooleanConstExpr::BooleanConstExpr(bool val, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	CharConst::CharConst(char val, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	BinaryExpr::BinaryExpr(BinaryOp op, Expr* lhs, Expr* rhs, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	UnaryExpr::UnaryExpr(UnaryOp op, Expr* expr, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	IfExpr::IfExpr(Expr* cond, Expr* body, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	WhileExpr::WhilExpr(Expr* cond, Expr* body, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	LoopExpr::LoopExpr(Expr* body, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	ForExpr::ForExpr(Expr* index, Expr* expr, Expr* body, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	MatchArm::MatchArm(Expr* name, Ident* value, Expr* body) : Decl(, pos) {
+
+	}
+	
+	MatchExpr::MatchExpr(Expr* cond, const std::vector<MatchArm*>& arms, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	DeclExpr::DeclExpr(Decl* decl) : Decl(, pos) {
+
+	}
+	
+	ParenthesisExpr::ParenthesisExpr(Expr* operand) : Decl(, pos) {
+
+	}
+	
+	SelectorExpr::SelectorExpr(Expr* operand, TypeSpec* element, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	BreakExpr::BreakExpr(mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	ContinueExpr::ContinueExpr(mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	ReturnExpr::ReturnExpr(const std::vector<Expr*> returns, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	CastExpr::CastExpr(Expr* expr, TypeSpec* ty, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	RangeExpr::RangeExpr(Expr* low, Expr* high, Expr* count, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	SliceExpr::SliceExpr(Expr* low, Expr* high, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	TupleIndexExpr::TupleIndexExpr(Expr* operand, i32 index, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	AssignmentExpr::AssignmentExpr(const std::vector<Expr*> lvalues, Expr* expr, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+	BlockExpr::BlockExpr(const std::vector<Expr*> elements, mist::Pos pos) : Decl(, pos) {
+
+	}
+	
+}
--- Mist/src/frontend/parser/ast/ast_expr.hpp
+++ Mist/src/frontend/parser/ast/ast_expr.hpp
@@ -3,10 +3,15 @@
 #include "ast_common.hpp"
 
 namespace ast {
+	struct Type;
+	struct Decl;
+	struct TypeSpec;
+
 	enum ExprKind {
 		// Represents an identifier.
 		Value,
-
+		Tuple,
+		
 		// Constants
 		IntegerConst,
 		FloatConst,
@@ -30,7 +35,247 @@ namespace ast {
 		Decl,
 		Parenthesis,
 		Selector,
-		
 
+		Break,
+		Continue,
+		Return,
+		Cast,
+		Range,
+		Slice,
+		TupleIndex,
+
+		Assignment,
+
+		Block
+	};
+
+	enum BinaryOp {
+		Plus,
+		Minus,
+		Slash, 
+		Percent,
+		Astrick,
+		AstrickAstrick,
+		LessLess,
+		GreaterGreater,
+		Pipe,
+		Carrot,
+		Less,
+		Greater,
+		LessEqual,
+		GreaterEqual,
+		EqualEqual,
+		BangEqual,
+	};
+
+	enum UnaryOp {
+		Minus,
+		Bang,
+		Tilde,
+		Ampersand,
+		Astrick
+	};
+
+	enum ConstantType {
+		I8,
+		I16,
+		I32,
+		I64,
+		U8,
+		U16,
+		U32,
+		U64,
+		F32,
+		F64,
+		Char
+	};
+
+	struct Expr {
+		ExprKind k;
+		Type* t;
+		mist::Pos p;
+
+		Expr(ExprKind k, mist::Pos p);
+
+		ExprKind kind();
+		Type* type();
+		mist::Pos pos();
+	};
+
+	struct ValueExpr : public Expr {
+		Ident* name;
+
+		Value(Ident* name);
+	};
+
+	struct TupleExpr : public Expr {
+		std::vector<Expr*> values;
+
+		Tuple(const std::vector<Expr*>& values, mist::Pos pos);
+	};
+
+	struct IntegerConstExpr : public Expr {
+		i64 value{0};
+		ConstantType cty;
+
+		IntegerConst(i64 val, ConstantType cty, mist::Pos pos);
+	};
+
+	struct FloatConstExpr : public Expr {
+		f64 value{0};
+		ConstantType cty;
+
+		FloatConst(f64 val, ConstantType cty, mist::Pos pos);
+	};
+
+	struct StringConstExpr : public Expr {
+		std::string value;
+
+		StringConst(const std::string& val, mist::Pos pos);
+	};
+
+	struct BooleanConstExpr : public Expr {
+		bool value;
+
+		BooleanConst(bool val, mist::Pos pos);
+	};
+
+	struct CharConstExpr : public Expr {
+		char value;
+
+		CharConst(char val, mist::Pos pos);
+	};
+
+	struct BinaryExpr : public Expr {
+		BinaryOp op;
+		Expr* lhs;
+		Expr* rhs;
+
+		BinaryExpr(BinaryOp op, Expr* lhs, Expr* rhs, mist::Pos pos);
+	};
+
+	struct UnaryExpr : public Expr {
+		UnaryOp op;
+		Expr* expr;
+		
+		UnaryExpr(UnaryOp op, Expr* expr, mist::Pos pos);
+	};
+
+	struct IfExpr : public Expr {
+		Expr* cond;
+		Expr* body;
+
+		IfExpr(Expr* cond, Expr* body, mist::Pos pos);
+	};
+
+	struct WhileExpr : public Expr {
+		Expr* cond;
+		Expr* body;
+
+		WhileExpr(Expr* cond, Expr* body, mist::Pos pos);
+	};
+
+	struct LoopExpr : public Expr {
+		Expr* body;
+
+		LoopExpr(Expr* body, mist::Pos pos);
+	};
+
+	struct ForExpr : public Expr {
+		Expr* index;
+		Expr* expr;
+		Expr* body;
+
+		ForExpr(Expr* index, Expr* expr, Expr* body, mist::Pos pos);
+	};
+
+	struct MatchArm {
+		Expr* name;
+		Ident* value;
+		Expr* body;
+
+		MatchArm(Expr* name, Ident* value, Expr* body);
+	};
+
+	struct MatchExpr : public Expr {
+		Expr* cond;
+		std::vector<MatchArm*> arms;
+
+		MatchExpr(Expr* cond, const std::vector<MatchArm*>& arms, mist::Pos pos);
+	};
+
+	struct DeclExpr : public Expr {
+		Decl* decl;
+
+		DeclExpr(Decl* decl);
+	};
+
+	struct ParenthesisExpr : public Expr {
+		Expr* operand;
+
+		ParenthesisExpr(Expr* operand);
+	};
+
+	struct SelectorExpr : public Expr {
+		Expr* operand;
+		TypeSpec* element;
+
+		SelectorExpr(Expr* operand, TypeSpec* element, mist::Pos pos);
+	};
+
+	struct BreakExpr : public Expr {
+		BreakExpr(mist::Pos pos);
+	};
+
+	struct ContinueExpr : public Expr {
+		ContinueExpr(mist::Pos pos);
+	};
+
+	struct ReturnExpr : public Expr {
+		std::vector<Expr*> returns;
+
+		ReturnExpr(const std::vector<Expr*> returns, mist::Pos pos);
+	};
+
+	struct CastExpr : public Expr {
+		Expr* expr;
+		TypeSpec* ty;
+
+		CastExpr(Expr* expr, TypeSpec* ty, mist::Pos pos);
+	};
+
+	struct RangeExpr : public Expr {
+		Expr* low;
+		Expr* high;
+		Expr* count;
+
+		RangeExpr(Expr* low, Expr* high, Expr* count, mist::Pos pos);
+	};
+
+	struct SliceExpr : public Expr {
+		Expr* low;
+		Expr* high;
+
+		SliceExpr(Expr* low, Expr* high, mist::Pos pos);
+	};
+
+	struct TupleIndexExpr : public Expr {
+		Expr* operand;
+		i32 index;
+
+		TupleIndexExpr(Expr* operand, i32 index, mist::Pos pos);
+	};
+
+	struct AssignmentExpr : public Expr {
+		std::vector<Expr*> lvalues;
+		Expr* expr;
+
+		AssignmentExpr(const std::vector<Expr*> lvalues, Expr* expr, mist::Pos pos);
+	};
+
+	struct BlockExpr : public Expr {
+		std::vector<Expr*> elements;
+
+		BlockExpr(const std::vector<Expr*> elements, mist::Pos pos);
 	};
 }
